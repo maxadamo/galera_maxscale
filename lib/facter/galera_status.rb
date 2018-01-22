e76616b1eb4194d695f2fb18eaa2f0264baa6ab9
Massimiliano Adamo <maxadamo@gmail.com>
2018-01-22 21:58:08 +0100

    minor

--- lib/facter/galera_status.rb
+++ lib/facter/galera_status.rb
@@ -2,7 +2,6 @@ Facter.add('galera_status') do
   setcode do
     require 'net/http'
     uri = URI('http://localhost:9200/')
-    res = Net::HTTP.get_response(uri)
-    res.code
+    Net::HTTP.get_response(uri).code
   end
 end
