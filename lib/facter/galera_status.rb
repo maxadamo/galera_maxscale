93385628b244274ee60487c74ee1c1101d27c870
Massimiliano Adamo <maxadamo@gmail.com>
2018-01-22 22:10:35 +0100

    testing different fact

--- lib/facter/galera_status.rb
+++ lib/facter/galera_status.rb
@@ -1,7 +1,13 @@
 Facter.add('galera_status') do
   setcode do
-    require 'net/http'
-    uri = URI('http://localhost:9200/')
-    Net::HTTP.get_response(uri).code
+    begin
+      Socket.tcp("localhost", 9200, connect_timeout: 5) {}
+    rescue
+      require 'net/http'
+      uri = URI('http://localhost:9200/')
+      Net::HTTP.get_response(uri).code
+    ensure
+      'UNKNOWN'
+    end
   end
 end
