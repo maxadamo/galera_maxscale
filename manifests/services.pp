04246a61c63903c6c0b31a9f8ebc79cf4b7da757
Massimiliano Adamo <maxadamo@gmail.com>
2018-01-29 20:04:37 +0100

    removed xinetd service and using module

--- manifests/services.pp
+++ manifests/services.pp
@@ -5,14 +5,6 @@
 #
 class galera_maxscale::services {
 
-  service { 'xinetd':
-    ensure     => running,
-    enable     => true,
-    hasrestart => true,
-    hasstatus  => true,
-    require    => Package['xinetd'];
-  }
-
   case $::osfamily {
     'RedHat': {
       if $::lsbmajdistrelease == '7' {
