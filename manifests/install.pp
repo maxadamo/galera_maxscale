7c6601f977c27ff8354431e40fb53f607b9cebca
Massimiliano Adamo <maxadamo@gmail.com>
2018-03-25 14:46:55 +0200

    code refactoring

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,26 +7,9 @@ class galera_maxscale::install (
   $percona_major_version = $::galera_maxscale::params::percona_major_version,
   ) inherits galera_maxscale::params {
 
-  xinetd::service { 'galerachk':
-    server         => '/usr/bin/clustercheck',
-    port           => '9200',
-    user           => 'root',
-    group          => 'root',
-    groups         => 'yes',
-    flags          => 'REUSE',
-    log_on_success => '',
-    log_on_failure => 'HOST',
-    require        => File[
-      '/usr/bin/clustercheck', '/root/.my.cnf', '/etc/sysconfig/clustercheck'
-    ];
-  }
-
   package {
     $other_pkgs:
       ensure => latest;
-    "Percona-Server-shared-compat-${percona_major_version}":
-      ensure => installed,
-      before => Package["Percona-XtraDB-Cluster-full-${percona_major_version}"];
     "Percona-XtraDB-Cluster-full-${percona_major_version}":
       ensure => installed;
   }
--- manifests/services.pp
+++ manifests/services.pp
@@ -5,6 +5,20 @@
 #
 class galera_maxscale::services {
 
+  xinetd::service { 'galerachk':
+    server         => '/usr/bin/clustercheck',
+    port           => '9200',
+    user           => 'root',
+    group          => 'root',
+    groups         => 'yes',
+    flags          => 'REUSE',
+    log_on_success => '',
+    log_on_failure => 'HOST',
+    require        => File[
+      '/usr/bin/clustercheck', '/root/.my.cnf', '/etc/sysconfig/clustercheck'
+    ];
+  }
+
   if $::lsbmajdistrelease == '7' {
     service { 'mariadb':
       ensure   => stopped,
